59fa27b6b3b2009510e0da3aa8e59b5ba2fb5c44
steve <stevemenezes70@gmail.com>
2020-05-23 06:39:18 -0400

    Login and Groupchat  components added

---
+++ src/components/Groupchat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Groupchat extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <div className="chatWindow"/>
+    }
+}
+
+export default Groupchat;
