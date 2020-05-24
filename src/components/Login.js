eb352429c1df8e24acce6ab54893cbd59dea4dc6
steve <stevemenezes70@gmail.com>
2020-05-24 02:48:57 -0400

    login in progress

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Popup from './Popup';
+
 
 class Login extends React.Component{
     constructor(props){
         super(props);
-        this.state= {};
+        this.state= {name: '',e_id: ''};
+        this.changeValue=this.changeValue.bind(this);
+        this.showValue=this.showValue.bind(this);
     }
 
+    changeValue(e){
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    showValue(e){
+        console.log("Though shall not name " + this.state.name);
+        console.log("Email as entered: " + this.state.e_id );
+        e.preventDefault();
+        e.target.reset();
+    }
     render(){
         return(
             <div className="App">
-                <h1>Login</h1>
+                <h2>Chat Room Login</h2>
+                <form onSubmit={this.showValue}>
+                    <div className="form-group">
+                        <label>
+                        Name:
+                        <input type="text" className="form-control" value={this.state.name} name="name" onChange={this.changeValue}/>
+                        </label>
+                    </div>    
+
+                    <input type="submit" value="Submit" className="btn btn-primary" />
+                </form>
             </div>
         );
     }
 }
+
 
 export default Login;
